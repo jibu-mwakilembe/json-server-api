6c404a152c82ad7403bed841b02ddc581afdc04f
jibu-mwakilembe <jibumwakilembe@gmail.com>
2025-01-02 07:07:00 -0800

    added render.yaml and server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+
+// Enable CORS
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+server.use(router);
+server.listen(process.env.PORT || 5000, () => {
+  console.log("JSON Server is running");
+});
